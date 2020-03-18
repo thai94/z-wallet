953cd52d956520284aeae8d227f4257ce7fa6478
thai <thainq@nec.vn>
2020-03-18 21:28:19 +0700

    add source code

---
+++ src/z-wallet/src/main/java/com/wallet/zwallet/ZWalletApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.zwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZWalletApplication.class, args);
+	}
+
+}
---
+++ src/z-wallet/src/test/java/com/wallet/zwallet/ZWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.zwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
